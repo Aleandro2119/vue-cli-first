22900eeae299d482debd924b61de647b12f9a346
Aleandro2119 <aleandro.basilico@gmail.com>
2022-02-07 18:04:34 +0100

    Add title

---
+++ src/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="title">
+    <h1>La mia prima app con Vue CLI!!!</h1>
+  </div>
+</template>
+
+<script>
+import Title from "./components/Title.vue";
+
+export default {
+  name: "Title",
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
